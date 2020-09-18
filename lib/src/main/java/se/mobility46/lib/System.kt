72ca3c0ab9837f2f64b14599463daaed065143e3
David Jobe <daavid.jobe@gmail.com>
2020-09-18 12:58:30 +0200

    fixed ua

--- lib/src/main/java/se/mobility46/lib/System.kt
+++ lib/src/main/java/se/mobility46/lib/System.kt
@@ -7,13 +7,31 @@ import java.util.*
 class System {
 
     enum class ErrorType {
+        PACKAGE_LABEL,
         APP_VERSION,
+        DEVICE_OS,
+        ANDROID_VERSION,
+        DEVICE_MODEL,
+        LANGUAGE
     }
 
     class SystemException(type: ErrorType) : Exception(type.toString())
 
     companion object {
 
+
+        private fun appName(ctx: Context) : String {
+            val nameId = ctx.applicationInfo.labelRes
+            if (nameId == 0) {
+                val nonLocalized = ctx.applicationInfo.nonLocalizedLabel
+                if (nonLocalized != null) {
+                    return nonLocalized.toString()
+                } else {
+                    throw SystemException(ErrorType.PACKAGE_LABEL)
+                }
+            }
+            return ctx.getString(nameId)
+        }
 
         private fun appVersion(ctx: Context) : String? {
             try {
@@ -24,9 +42,10 @@ class System {
             }
         }
 
-        fun description(context: Context, appName: String) : Result<String> {
+        fun description(context: Context) : Result<String> {
             return Result {
 
+                val appName = appName(context)
                 val appVersion = appVersion(context)
                 val deviceOS = "Android"
                 val androidVersion = android.os.Build.VERSION.RELEASE
@@ -39,7 +58,7 @@ class System {
                     language = "sv"
                 }
 
-                return@Result "$appName/$appVersion; $deviceOS/$androidVersion; Android; model=$deviceModel; lang=$language;"
+                return@Result "$appName/$appVersion; $deviceOS/$androidVersion; model=$deviceModel; lang=$language;"
             }
         }
     }
